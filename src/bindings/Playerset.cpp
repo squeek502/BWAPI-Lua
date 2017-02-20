5bea48fe6b9fbb3cf6d2adcf45539fbc1ed79b59
squeek <squeek502@hotmail.com>
2017-02-19 19:34:46 -0800

    Finish Playerset bindings

--- src/bindings/Playerset.cpp
+++ src/bindings/Playerset.cpp
@@ -8,7 +8,15 @@ namespace BWAPI_Lua
 {
 	void bindPlayerset(sol::table module)
 	{
-		auto userTypePlayerset = module.create_simple_usertype<Playerset>();
+		auto userTypePlayerset = module.create_simple_usertype<Playerset>(
+			"getUnits", &Playerset::getUnits,
+			"getRaces", &Playerset::getRaces,
+			"setAlliance", sol::overload(
+				[](Playerset& set) { return set.setAlliance(); },
+				[](Playerset& set, bool allies) { return set.setAlliance(allies); },
+				&Playerset::setAlliance
+			)
+		);
 		bindSetContainer<Playerset, Player>(userTypePlayerset);
 		module.set_usertype("Playerset", userTypePlayerset);
 	}
